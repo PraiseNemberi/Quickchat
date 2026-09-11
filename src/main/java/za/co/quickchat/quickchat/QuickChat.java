b8575bcdec8d4c5a3e595bb4e9685cc868e6bdee
Praise Ray Nemberi <st10542243@rcconnect.edu.za>
2026-09-11 02:10:36 +0200

    Add keyboard input for registration details

--- src/main/java/za/co/quickchat/quickchat/QuickChat.java
+++ src/main/java/za/co/quickchat/quickchat/QuickChat.java
@@ -2,6 +2,7 @@
 
 package za.co.quickchat.quickchat;
 
+import java.util.Scanner;
 /**
  *
  * @author Praise Nemberi ST10542243
@@ -9,7 +10,21 @@ package za.co.quickchat.quickchat;
 public class QuickChat {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to QuickChat");
-        System.out.println("Created by Praise Ray Nemberi");
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a username: ");
+            String username = input.nextLine();
+            
+            System.out.print("Enter a Password: ");
+            String password = input.nextLine();
+            
+            System.out.print("Enter your cell number: ");
+            String cellNumber = input.nextLine();
+            
+            
+            
+            System.out.println("Username: " + username);
+            System.out.println("Password: "  + password);
+            System.out.println("Cell number: " + cellNumber);
+        }
     }
 }
